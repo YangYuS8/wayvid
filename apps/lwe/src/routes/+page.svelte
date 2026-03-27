29d1dd296f0d71e1778c7c4a9fecf4da4615ccb0
YangYuS8 <YangYuS8@163.com>
2026-03-27 11:18:34 +0800

    chore: make svelte shell scaffold truthful

--- apps/lwe/src/routes/+page.svelte
+++ apps/lwe/src/routes/+page.svelte
@@ -6,3 +6,11 @@
     void goto('/library');
   });
 </script>
+
+<svelte:head>
+  <title>LWE</title>
+</svelte:head>
+
+<h1>LWE</h1>
+<p>Opening the Library shell.</p>
+<p><a href="/library">Continue to Library</a></p>
